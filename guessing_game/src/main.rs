7d40113a84bf7bec4f0b1236df2f99b19ab6dd31
Salil Shankar <Salil@Salils-MacBook-Pro.local>
2021-02-08 11:36:12 +0530

    adding all files that are created so far

---
+++ guessing_game/src/main.rs
@@ -0,0 +1,44 @@
+use std::io;
+use rand::Rng;
+use std::cmp::Ordering;
+
+fn main() {
+    let secret_number = rand::thread_rng().gen_range(0..101);
+    
+    loop {
+        println!("Guess the number");
+
+        let mut guess = String::new();
+
+        
+        // println!("system hacker says: {}", secret_number);
+
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line!");
+
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => {
+                println!("invalid input");
+                println!();
+                continue;
+            },
+        };
+        println!("You guessed {}", guess);
+
+        
+
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal => {
+                println!("You win!");
+                break;
+            }
+        }
+
+        println!("system had {}", secret_number);
+        println!();
+    }
+}
